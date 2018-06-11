075dc2902e95821c22d9c8fbf0da2424451233c8
Leonard Pauli <leonardpauli@me.com>
2018-06-11 08:41:36 +0200

    arrayDeltaActions: proper tests, WIP logic

--- src/utils/arrayDeltaActions.js
+++ src/utils/arrayDeltaActions.js
@@ -6,12 +6,16 @@
 
 /* eslint brace-style:0, max-statements:0 */
 const arrayDeltaActions = ({move, add, remove})=> (as, bs)=> {
-	const rs = as.slice(); let offset = 0
+	const rs = as.slice(); let offset = 0; let offsetAddBefore = 0
+	const gs = as.map(_=> false) // ghosts
 	let ai = 0; let a = void 0
 	let bi = 0; let b = void 0
 	do {
+		while (gs[ai] && ai < as.length) { ai++; offsetAddBefore-- } // skip ghosts
 		a = ai < as.length? as[ai]: void 0
 		b = bi < bs.length? bs[bi]: void 0
+		// console.log(a, b, ';', ai, bi, gs)
+		// console.log(rs)
 		if (a===b) {
 			if (a===void 0) break
 			ai++
@@ -19,18 +23,34 @@ const arrayDeltaActions = ({move, add, remove})=> (as, bs)=> {
 		} else if (b!==void 0) {
 			// find b in as
 			let i=ai+1; let found = false
-			do {
+			while (i < as.length) {
+				while (gs[i] && i < as.length) i++ // skip ghosts
+				if (i >= as.length) break
 				const x = as[i]
 				if (x===b) { found = true; break }
 				i++
-			} while (i < as.length)
+			}
 			// move from later in as if found, else add from bs
-			if (found) move({abs: ai, rel: ai + offset}, {abs: i, rel: i + offset, x: as[i]})
-			else add({abs: ai, rel: ai + offset}, {x: b, i: bi})
-			offset += 1
+			if (found) {
+				const to = {abs: ai + (offset+offsetAddBefore), rel: ai + (offset+offsetAddBefore)}
+				const fr = {abs: i, rel: i + (offset+offsetAddBefore), x: as[i]}
+				// console.log(offset, offsetAddBefore)
+				move(to, fr)
+				gs[i] = true
+				rs.splice(fr.rel, 1)
+				rs.splice(to.abs, 0, fr.x)
+				// offset++
+				offsetAddBefore++
+			} else {
+				add({abs: ai + offsetAddBefore, rel: ai + (offset+offsetAddBefore)}, {x: b, i: bi})
+				rs.splice(ai + (offset+offsetAddBefore), 0, b)
+				offset++
+			}
 			bi++
 		} else {
-			remove({abs: ai, rel: ai + offset, x: a}); offset--
+			remove({abs: ai, rel: ai + (offset+offsetAddBefore), x: a})
+			rs.splice(ai + (offset+offsetAddBefore), 1)
+			offset--
 			ai++
 		}
 	} while (true) // eslint-disable-line no-constant-condition
--- src/utils/arrayDeltaActions.test.js
+++ src/utils/arrayDeltaActions.test.js
@@ -7,18 +7,83 @@
 import arrayDeltaActions from './arrayDeltaActions'
 import {log} from 'string-from-object'
 
-describe('arrayDeltaActions', ()=> {
-	const diffit = (aa, bb)=> expectedDiff=> {
-		const as = aa.split(',')
-		const bs = bb.split(',')
-		const diff = []; arrayDeltaActions({
-			add: (i, x)=> diff.push(`add at [${i.rel}]/[${i.abs}]: ${x.x} == bs[${x.i}]`),
-			move: (i, f)=> diff.push(`move to [${i.rel}]/[${i.abs}] from [${f.rel}]/[${f.abs}] // ${f.x}`),
-			remove: i=> diff.push(`remove at [${i.rel}]/[${i.abs}] // ${i.x}`),
-		})(as, bs)
-		return diff
+// https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+const arrayShuffle = xs=> {
+	let i = xs.length
+	while (i > 0) { // While there are elements in the xs
+		const j = Math.floor(Math.random() * i); i-- // get random index + dec counter
+		const xc = xs[i]; xs[i] = xs[j]; xs[j] = xc // swap the last element with it
 	}
-	it('a,ab', ()=> {
-		log(diffit('a', 'ab')())
-	})
+	return xs
+}
+
+const rndstr = ()=> (s=>	arrayShuffle(s.split('')).join('')
+	.substr(0, Math.floor(Math.random()*s.length))
+)('abcd') // efghijklmnopqrstuvwxyz
+
+describe('arrayDeltaActions', ()=> {
+	const diffit = (expr, name, doLog)=> {
+		const [aa, bb] = expr.split(',')
+		const as = aa.split('')
+		const bs = bb.split('')
+		const rs = as.slice()
+		const diff = []; arrayDeltaActions({
+			add: (i, x)=> (
+				rs.splice(i.rel, 0, bs[x.i]),
+				diff.push(`add '${x.x}' (bs[${x.i}]) at r[${i.rel}] (final[${i.abs}])`)),
+			move: (i, f)=> (
+				0 && console.log(rs), rs.splice(i.rel, 0, rs[f.rel]), 0 && console.log(rs), rs.splice(f.rel+1, 1), 0 && console.log(rs),
+				diff.push(`move '${f.x}' (r[${f.rel}], org[${f.abs}]) to r[${i.rel}] (final[${i.abs}])`)),
+			remove: i=> (
+				rs.splice(i.rel, 1),
+				diff.push(`remove '${i.x}' at r[${i.rel}] (org[${i.abs}])`)),
+		})(as, bs)
+		doLog && log(diff)
+		it(expr+' // '+name, ()=> expect(rs).toEqual(bs))
+	}
+	diffit('a,ab', 'append, 1')
+	diffit('a,ba', 'prepend, 1')
+	diffit('ab,acb', 'insert')
+	
+	diffit('a,abc', 'append, 2')
+	diffit('a,cba', 'prepend, 2')
+	
+	diffit('a,', 'remove, all 1')
+	diffit('ab,b', 'remove, first 1')
+	diffit('ab,a', 'remove, last 1')
+
+	diffit('abc,c', 'remove, first 2')
+	diffit('abc,bc', 'remove, first 1 of 3', true)
+	diffit('abc,a', 'remove, last 2')
+	diffit('abc,ab', 'remove, last 1 of 3')
+
+	diffit('abc,acb', 'move, switch')
+	diffit('abc,cab', 'move, back')
+	diffit('abc,bca', 'move, forward')
+	diffit('abc,ca', '-')
+
+	diffit('ba,ac', 'remove + add')
+	diffit('dab,cad', 'move, add, and remove')
+	diffit('bca,adc', 'move, add, and remove', true)
+
+	/*
+	bca
+	abc a2,2;0 -> 0,0
+	adbc d-,-;1 *> 1,1
+	adcb c3,1;2 -> 2,2
+	adc b4,0;x x
+	adc
+	*/
+
+	// diffit('bcd,dc', 'move, add, and remove', true)
+
+	// diffit(`${rndstr()},${rndstr()}`, 'random')
+	// diffit(`${rndstr()},${rndstr()}`, 'random')
+	// diffit(`${rndstr()},${rndstr()}`, 'random')
+	// diffit(`${rndstr()},${rndstr()}`, 'random')
+	// diffit(`${rndstr()},${rndstr()}`, 'random')
+	// diffit(`${rndstr()},${rndstr()}`, 'random')
+	// diffit(`${rndstr()},${rndstr()}`, 'random')
+	// diffit(`${rndstr()},${rndstr()}`, 'random')
+	// diffit(`${rndstr()},${rndstr()}`, 'random')
 })
